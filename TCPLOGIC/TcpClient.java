9d8c6f53658399dcf025fb031133195c6ac5732a
kokanevina <kokane.vina@gmail.com>
2022-08-29 13:44:15 +0530

    TCP chat

--- TCPLOGIC/TcpClient.java
+++ TCPLOGIC/TcpClient.java
@@ -1,23 +1,22 @@
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class TcpClient {
-	
+	DataOutputStream outputStream;
 	InetAddress serveraddress;
 	
 	public TcpClient() throws UnknownHostException {
 		System.out.println("Connection Establishment");
-		serveraddress=InetAddress.getLocalHost();
+		serveraddress=InetAddress.getByName("DESKTOP-RBMTGN6");
 		System.out.println(serveraddress);
 		System.out.println("Client Establishing Connection with Server");
 		try(Socket clientSocket=new Socket(serveraddress,2020)){
-			
-			
-			
-			
+			outputStream =new DataOutputStream(clientSocket.getOutputStream());
+			chat();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -25,8 +24,12 @@ public class TcpClient {
 		System.out.println("Connection gets autoclosed");
 		
 
-	}
+	}// constructor
 	
+	public void chat() throws IOException{
+		String clientMessage="Hello How are you?";
+		outputStream.writeUTF(clientMessage);
+	}
 	
 	public static void main(String[] args) {
 		try {
--- TCPLOGIC/TcpServer.java
+++ TCPLOGIC/TcpServer.java
@@ -1,25 +1,31 @@
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpServer {
-		Socket socket;
-	
-		
+		Socket socket;	
+		DataInputStream inputStream;
 		public TcpServer() {
-			System.out.println("Connection Establishment with Client");
-			
+			System.out.println("Connection Establishment with Client");		
 			try(ServerSocket ss=new ServerSocket(2020);    ){
 				System.out.println("Server is waiting for client request...... ");
 				socket=ss.accept();
 				System.out.println("Connection Established...........");
+				inputStream=new DataInputStream(socket.getInputStream());
+				chat();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		
+		public void chat() throws IOException{
+			String messagefromClient=inputStream.readUTF();
+			System.out.println(messagefromClient);
+		}
 		public static void main(String[] args) {
 			TcpServer server=new TcpServer();
 			
